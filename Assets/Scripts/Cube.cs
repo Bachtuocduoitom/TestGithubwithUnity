8c69444eac1cb86a5ec7dde8e92f0e855ba9eb20
HieuTM <davidjohnbachtuoc@gmail,com>
2024-03-25 09:53:12 +0700

    ! init project

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log("hi>>>");
+    }
+}
